9e27c5a054d8b59eb62f078f807ff572162ac794
AlankarSaxena2712 <saxenaalankar42@gmail.com>
2021-04-18 11:34:32 +0530

    created clone with local variables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { InputLabel, Input } from '@material-ui/core';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Message from './Message';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([
+    {username: "alankar", text:"hey giyd"},
+    {username: "shaily", text:"wahts uo"}
+  ]);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    setUsername(prompt("Enter your username : "));
+    }, [])
+
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([...messages, {username: username, text: input}]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Helo</h1>
+      <form>
+        <FormControl>
+          <InputLabel>Enter Message...</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)}/>
+          <Button disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>Send Message</Button>
+        </FormControl>
+      </form>
+
+      {
+        messages.map(message => (
+          <Message message={message} username={username} />
+        ))
+      }
+
     </div>
   );
 }
